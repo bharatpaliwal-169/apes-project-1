a0085b210bfda6d74aed7c32ced2aa6595200095
Bharat Paliwal <18uec169@lnmiit.ac.in>
2021-03-23 22:36:47 +0530

    initial project and plugin setup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './test/reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.css';
 
 ReactDOM.render(
   <React.StrictMode>
