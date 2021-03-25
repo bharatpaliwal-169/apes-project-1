9c8f7459d39b45cae229f4d730a0eb2bcf834e85
Bharat Paliwal <18uec169@lnmiit.ac.in>
2021-03-25 22:53:55 +0530

    initial design done

--- src/components/Elements/Item.js
+++ src/components/Elements/Item.js
@@ -2,12 +2,8 @@ import React from 'react';
 
 const Item = (props) => {
   return (
-          <div class='col-5 col-sm-3'>
-            {/* <div className="card" key={props.id} >
-              <div className='card-body'> */}
-                <img src={props.img} alt='{item.title}' className='img-fluid card-img-top mt-2 mb-2 p-2' />
-              {/* </div>
-            </div> */}
+          <div className='col-5 col-sm-3' key={props.id}>
+            <img key={props.id} src={props.img} alt='{item.title}' className='img-fluid card-img-top mt-2 mb-2 p-2' />
           </div>
         )
 }
